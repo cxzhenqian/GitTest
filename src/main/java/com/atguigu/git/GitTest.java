b9ff84c29a755f749f22770e57873c3232bae892
qian <cxzhenqian@163.com>
2022-05-04 14:22:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Zhenqian Zhang
+ * @create 2022-05-04 14:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
